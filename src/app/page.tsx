acf1b96d5242f635abcda9859a581cba040cbba6
Ahmed Mansour <133324agh@gmail.com>
2024-04-16 09:19:14 +0200

    1.00

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import Link from "next/link";
+import Image from "next/image";
 
+const Glrey = [
+  "https://utfs.io/f/693f7f7a-0127-4feb-98bb-7a78accf6ddd-2dxv1y.jpg",
+  "https://utfs.io/f/53cefbc1-26c0-45eb-a3af-b5c0d9e745b6-ws2jhm.jpg",
+  "https://utfs.io/f/ba111a6c-0246-451a-a570-82264ebed7e8-lnpi92.jpg",
+]
+
+const OG = Glrey.map((URL, index) => (({
+  id: index,
+  URL,
+})))
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-    
+    <main className=" bg-black text-white">
+      <div className="flex flex-wrap p-4 gap-5 justify-center">
+        {
+          [...OG, ...OG, ...OG].map((e) => (
+            <div key={e.id}>
+              <Image src={e.URL} alt={""} width={200} height={200} />
+            </div>
+          ))
+        }
+      </div>
     </main>
   );
 }
---
+++ src/components/Nav/Navbar.tsx
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+  return <nav className="h-12 px-5 flex justify-between items-center text-2xl">
+    <h1>
+        Logo
+    </h1>
+    <p>
+      Sgin In  
+    </p>
+  </nav>
+}
+
+export default Navbar
